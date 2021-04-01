0bdd36308d632af380afead5ab09a63a1bb6a339
Bernardo <bernardo.giron99@gmail.com>
2021-04-01 00:36:50 +0000

    Add matrix animations and other controls

--- src/main/webapp/scripts/matrixController.js
+++ src/main/webapp/scripts/matrixController.js
@@ -1,32 +1,179 @@
 var rowOpening = "<tr>";
 var rowClosure = "</tr>";
 var cellZoom = "style = \"width: 3em; height: 3em\"";
-var alive = "<td class = \"matrixCell\"><img src = \"../images/alive normal cell.png\" " + cellZoom+"class = \"table-image\"></td>";
-var death = "<td class = \"matrixCell\"><img src = \"../images/dead cell.png\" "+cellZoom+"class = \"table-image\"></td>";
+var alive = "<td class = \"matrixCell\"><img src = \"../images/alive normal cell.png\" " + cellZoom+"class = \"table-image\" ";
+var death = "<td class = \"matrixCell\"><img src = \"../images/dead cell.png\" "+cellZoom+"class = \"table-image\" ";
+var cellClosure = "></td>";
 var rows = 0;
 var columns = 0;
-var matrix = "";
+var selectedCellType = "deadCell"
+var matrix = [[0,0,0],[0,0,0],[0,0,0]];
+var playSpeed = 1000;
+var cellWithMask = false;
+var cellWithVaccine = false;
+var playing = false;
+var timeController = null;
+var cellDictionary ={
+    0: "../images/dead cell.png",
+    1: "../images/alive normal cell.png",
+    2: "../images/alive mask.png",
+    3: "../images/alive vaccine.png",
+    4: "../images/alive mask vaccine.png",
+    5: "../images/Infected Cell.png",
+    6: "../images/infected mask.png",
+    7: "../images/infected vaccine.png",
+    8: "../images/infected mask vaccine.png"
+};
 
+function generateRandomMatrix(){
+    for(let row = 0; row < rows; row++){
+        for(let column = 0 ; column < columns ; column++ ){
+            document.getElementById(row.toString() + column.toString()).src = cellDictionary[ Math.floor( Math.random() * 8 ) ];
+        }
+    }
+    gameLogic();
+}
+
+function gameLogic(){
+    if(playing){
+        timeController = setTimeout(() => {
+                                                if(playing){
+                                                    generateRandomMatrix();
+                                                }
+                                            }, playSpeed);
+    }
+}
+
+function startTheGame(){
+    if(!playing){
+        playing = true;
+        gameLogic();
+    }
+}
+
+function stopTheGame(){
+    playing = false;
+    clearInterval(timeController);
+
+}
+
+function updateCellModifier(target){
+    if(target == "mask")
+        cellWithMask = !cellWithMask;
+    else
+        cellWithVaccine = !cellWithVaccine;
+    console.log(cellWithMask);
+    console.log(cellWithVaccine);
+}
 
 function updateMatrix(){
-
+    matrix = []
     rows = document.getElementById("matrixRows").value;
     columns = document.getElementById("matrixColumns").value;
-    matrix = "";
+    let matrixToTable = "";
+    let updateFunction = "onclick = \"updateMatrixCell(this)\"";
     for(let row = 0; row < rows; row++){
-        matrix += rowOpening;
+        let tempRow = []
+        matrixToTable += rowOpening;
         for(let column = 0; column < columns; column++){
-            matrix += death;
+            tempRow.push(0)
+            matrixToTable += death + "id = \"" + row.toString() + column.toString() + "\" " + updateFunction + cellClosure;
         }
-        matrix += rowClosure;
+        matrix.push(tempRow);
+        matrixToTable += rowClosure;
     }
-    document.getElementById("matrixTable").innerHTML = matrix;
+    document.getElementById("matrixTable").innerHTML = matrixToTable;
+}
+
+
+function updateMatrixCell(object){
+    let row = parseInt( object.id[0] );
+    let column = parseInt( object.id[1] );
+    switch( selectedCellType ){
+        case "deadCell":
+            object.src = "../images/dead cell.png";
+            matrix[row][column] = 0;
+            break;
+        
+        case "aliveCell":
+            if (!cellWithMask && !cellWithVaccine){
+                object.src = "../images/alive normal cell.png";
+                matrix[row][column] = 1;
+            }else if (cellWithMask && cellWithVaccine){
+                object.src = "../images/alive mask vaccine.png";
+                matrix[row][column] = 4;
+            }else if (cellWithMask){
+                object.src = "../images/alive mask.png";
+                matrix[row][column] = 2;
+            }else{
+                object.src = "../images/alive vaccine.png";
+                matrix[row][column] = 3;
+            }
+            
+            break;
+        
+        case "infectedCell":
+            if (!cellWithMask && !cellWithVaccine){
+                object.src = "../images/Infected Cell.png";
+                matrix[row][column] = 5;
+            }else if (cellWithMask && cellWithVaccine){
+                object.src = "../images/infected mask vaccine.png";
+                matrix[row][column] = 8;
+            }else if (cellWithMask){
+                object.src = "../images/infected mask.png";
+                matrix[row][column] = 6;
+            }else{
+                object.src = "../images/infected vaccine.png";
+                matrix[row][column] = 7;
+            }
+            break;
+    }
+}
+
+function updateSelectedCellType(object){
+    selectedCellType = object.id;
+    switch(selectedCellType)
+    {
+        case "deadCell":
+            object.src = "../images/dead selected.png";
+            document.getElementById("aliveCell").src = "../images/alive normal cell.png";
+            document.getElementById("infectedCell").src = "../images/Infected Cell.png";
+            break;
+        
+        case "aliveCell":
+            object.src = "../images/live selected.png";
+            document.getElementById("deadCell").src = "../images/dead cell.png";
+            document.getElementById("infectedCell").src = "../images/Infected Cell.png";
+            break;
+        
+        case "infectedCell":
+            object.src = "../images/infected selected.png";
+            document.getElementById("deadCell").src = "../images/dead cell.png";
+            document.getElementById("aliveCell").src = "../images/alive normal cell.png";
+            break;
+    }
+}
+
+function updateSpeed(object){
+    playSpeed = 1000 - ( (parseInt(object.value) - 1) * 100 );
+    document.getElementById("matrixSpeedLabel").innerText = "Speed: " + object.value.toString();
 }
 
 function updateZoom(){
     let newZoomLevel = document.getElementById("matrixZoom").value;
+    document.getElementById('matrixZoomLabel').innerText = "Zoom: " + (newZoomLevel - 2).toString();
     cellZoom = "style = \"width: "+newZoomLevel+"em; height: "+newZoomLevel+"em\"";
-    alive = "<td class = \"matrixCell\"><img src = \"../images/alive normal cell.png\" " + cellZoom+"class = \"table-image\"></td>";
-    death = "<td class = \"matrixCell\"><img src = \"../images/dead cell.png\" "+cellZoom+"class = \"table-image\"></td>";
-    updateMatrix();
+    alive = "<td class = \"matrixCell\"><img src = \"../images/alive normal cell.png\" " + cellZoom+"class = \"table-image\" ";
+    death = "<td class = \"matrixCell\"><img src = \"../images/dead cell.png\" "+cellZoom+"class = \"table-image\" ";
+
+    for(let row = 0; row < rows; row++){
+        for( let column = 0; column < columns; column++){
+
+            let cell = document.getElementById(row.toString() + column.toString());
+
+            cell.style['width'] = newZoomLevel.toString() + "em";
+            cell.style['height'] = newZoomLevel.toString() + "em";
+
+        }
+    }
 }
